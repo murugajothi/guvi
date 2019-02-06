4015621c79ec7aad5309f5537d73b39f4e3c4722
murugajothi <47380957+murugajothi@users.noreply.github.com>
2019-02-06 16:09:41 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,11 @@
+
+		import java.util.Scanner;
+class OddOrEven
+{
+public static void main(String args[])
+{
+int x;
+System. out. println("Enter an integer to check if it is odd or even");
+Scanner in = new Scanner(System. in);
+	}
+}
