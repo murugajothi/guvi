d8beff4dc4ca9cf89d55e89d3db5036e27163433
murugajothi <47380957+murugajothi@users.noreply.github.com>
2019-02-06 14:45:42 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) 
+	{
+	int a;
+	Scanner S=new Scanner(System.in);
+	System.out.println("");
+	a=S.nextInt();
+	if(a>0)
+	{
+	System.out.println("positive");
+	}
+	else  if(a==0) 
+	{
+
+	System.out.println("Zero");
+	}
+	else
+	{
+	System.out.println("Neagtive");
+	}
+}
+}
+	
+	
